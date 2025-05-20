75907a6abfa3b21f50ee6d4a88a9dd12028de93b
robin025 <rsingh8@umassd.edu>
2025-05-20 19:49:50 -0400

    feat: add navbar and global css variables

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import routes from './routes';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 const App: React.FC = () => {
+  const handleSearch = (searchTerm: string) => {
+    console.log('Searching for:', searchTerm);
+    // Implement search functionality here
+  };
+
   return (
     <div className="App">
       <Router>
-        <Routes>
-          {routes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<route.component />}
-            />
-          ))}
-        </Routes>
+        <Navbar onSearch={handleSearch} />
+        <div className="content-container">
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={<route.component />}
+              />
+            ))}
+          </Routes>
+        </div>
       </Router>
     </div>
   );
